cdeb1ac755cb8775da0b3e9b863860de5629a87b
José Luis Olivares Rojas <olivares.rojas.jose@gmail.com>
2019-10-10 16:20:01 -0500

    Login enhancement

--- AmbienteWeb.Proyecto.Carpooling.Web/Areas/Administrator/Controllers/HomeController.cs
+++ AmbienteWeb.Proyecto.Carpooling.Web/Areas/Administrator/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace AmbienteWeb.Proyecto.Carpooling.Web.Areas.Administrator.Controllers
 {
-    [Authorize(Roles = "Administrator")]
+    [Authorize()]
     public class HomeController : Controller
     {
         // GET: Administrator/Home
--- AmbienteWeb.Proyecto.Carpooling.Web/Models/IdentityModels.cs
+++ AmbienteWeb.Proyecto.Carpooling.Web/Models/IdentityModels.cs
@@ -21,8 +21,19 @@ namespace AmbienteWeb.Proyecto.Carpooling.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("SecurityConnection", throwIfV1Schema: false)
         {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>().ToTable("User");
+            modelBuilder.Entity<IdentityRole>().ToTable("Role");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("UserRole");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("UserClaim");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("UserLogin");
         }
 
         public static ApplicationDbContext Create()
